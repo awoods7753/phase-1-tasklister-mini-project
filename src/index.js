0cbe81379e4ee78f18ce9359c76f3fa3d0afcecf
Austin <awoods7753@gmail.com>
2022-03-06 17:51:04 -0500

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,21 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let form = document.querySelector('create-task-form')
+  let form = document.querySelector('#create-task-form')
   form.addEventListener('submit', (e) => {
     e.preventDefault()
-    toDoList(e.target.new-task-description.value)
+    console.log(e.target)
+    toDoList(e.target.new_task_description.value)
     form.reset()
   })
 });
 
 function toDoList(list) {
-  let p = document.createElement('p')
+  let p = document.createElement('li')
   let button = document.createElement('button')
   button.addEventListener('click', handleDelete)
   button.textContent = 'Delete'
   p.textContent = `${list}`
   p.appendChild(button)
   console.log(p)
-  document.querySelector("tasks").appendChild(p)
+  document.querySelector("#tasks").appendChild(p)
 }
 
 function handleDelete(e){
