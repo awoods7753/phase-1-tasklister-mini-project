bc067c2aeaaf9c09c32d8506be6c2fd7d692e26f
Austin <awoods7753@gmail.com>
2022-03-06 17:24:22 -0500

    Working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    toDoList(e.target.new-task-description.value)
+    form.reset()
+  })
 });
+
+function toDoList(list) {
+  let p = document.createElement('p')
+  let button = document.createElement('button')
+  button.addEventListener('click', handleDelete)
+  button.textContent = 'Delete'
+  p.textContent = `${list}`
+  p.appendChild(button)
+  console.log(p)
+  document.querySelector("tasks").appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
